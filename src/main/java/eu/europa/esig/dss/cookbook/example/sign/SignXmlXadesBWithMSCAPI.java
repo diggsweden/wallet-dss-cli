b9f04383001029f74b98d17c292e773674ee372c
Eric Thelin <me@ericthelin.se>
2026-07-21 16:25:35 +0200

    feat: copy example form DSS Cookbook
    See: https://github.com/esig/dss/blob/master/dss-cookbook/src/main/java/eu/europa/esig/dss/cookbook/example/sign/SignXmlXadesBWithMSCAPI.java
    Signed-off-by: Eric Thelin <extern.eric.thelin@digg.se>

---
+++ src/main/java/eu/europa/esig/dss/cookbook/example/sign/SignXmlXadesBWithMSCAPI.java
@@ -0,0 +1,89 @@
+// SPDX-FileCopyrightText: 2015 European Commission, provided under the CEF programme
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+package eu.europa.esig.dss.cookbook.example.sign;
+
+import eu.europa.esig.dss.enumerations.DigestAlgorithm;
+import eu.europa.esig.dss.enumerations.SignatureLevel;
+import eu.europa.esig.dss.enumerations.SignaturePackaging;
+import eu.europa.esig.dss.model.DSSDocument;
+import eu.europa.esig.dss.model.FileDocument;
+import eu.europa.esig.dss.model.SignatureValue;
+import eu.europa.esig.dss.model.ToBeSigned;
+import eu.europa.esig.dss.token.DSSPrivateKeyEntry;
+import eu.europa.esig.dss.token.MSCAPISignatureToken;
+import eu.europa.esig.dss.spi.validation.CommonCertificateVerifier;
+import eu.europa.esig.dss.xades.XAdESSignatureParameters;
+import eu.europa.esig.dss.xades.signature.XAdESService;
+
+import java.util.List;
+
+/**
+ * How to sign using MS-CAPI.
+ */
+public class SignXmlXadesBWithMSCAPI {
+
+  /**
+   * Executable application
+   */
+  private SignXmlXadesBWithMSCAPI() {}
+
+  /**
+   * Main method
+   *
+   * @param args not applicable
+   * @throws Exception if an exception occurs
+   */
+  public static void main(String[] args) throws Exception {
+    // GET document to be signed -
+    // Return DSSDocument toSignDocument
+    DSSDocument toSignDocument = new FileDocument("src/main/resources/xml_example.xml");
+
+    // Creation of MS-CAPI signature token
+    try (MSCAPISignatureToken signingToken = new MSCAPISignatureToken()) {
+
+      List<DSSPrivateKeyEntry> list = signingToken.getKeys();
+      // Choose the right private key entry from store.
+      // The index will depend of the number of the certificates on your card.
+      System.out.println(list.size());
+      DSSPrivateKeyEntry privateKey = list.get(0);
+
+      // Preparing parameters for the PAdES signature
+      XAdESSignatureParameters parameters = new XAdESSignatureParameters();
+      // We choose the level of the signature (-B, -T, -LT, -LTA).
+      parameters.setSignatureLevel(SignatureLevel.XAdES_BASELINE_B);
+      // We choose the type of the signature packaging (ENVELOPING, DETACHED).
+      parameters.setSignaturePackaging(SignaturePackaging.ENVELOPED);
+      // We set the digest algorithm to use with the signature algorithm. You must use the
+      // same parameter when you invoke the method sign on the token.
+      parameters.setDigestAlgorithm(DigestAlgorithm.SHA256);
+
+      // We set the signing certificate
+      parameters.setSigningCertificate(privateKey.getCertificate());
+      // We set the certificate chain
+      parameters.setCertificateChain(privateKey.getCertificateChain());
+
+      // Create common certificate verifier
+      CommonCertificateVerifier commonCertificateVerifier = new CommonCertificateVerifier();
+      // Create CAdES xadesService for signature
+      XAdESService xadesService = new XAdESService(commonCertificateVerifier);
+
+      // Get the SignedInfo segment that need to be signed.
+      ToBeSigned dataToSign = xadesService.getDataToSign(toSignDocument, parameters);
+
+      // This function obtains the signature value for signed information using the
+      // private key and specified algorithm
+      DigestAlgorithm digestAlgorithm = parameters.getDigestAlgorithm();
+      SignatureValue signatureValue = signingToken.sign(dataToSign, digestAlgorithm, privateKey);
+
+      // We invoke the xadesService to sign the document with the signature value obtained in
+      // the previous step.
+      DSSDocument signedDocument =
+          xadesService.signDocument(toSignDocument, parameters, signatureValue);
+
+      // save the signed document on the filesystem
+      signedDocument.save("target/signedXmlXadesMSCapi.xml");
+    }
+  }
+
+}
